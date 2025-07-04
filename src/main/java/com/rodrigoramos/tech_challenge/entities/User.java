9c7707dbec1e14f56241be9c6c9e471139780167
Rodrigo Ramos <digojramos@gmail.com>
2025-07-04 14:33:33 -0300

    feat: create User entity

---
+++ src/main/java/com/rodrigoramos/tech_challenge/entities/User.java
@@ -0,0 +1,69 @@
+package com.rodrigoramos.tech_challenge.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String login;
+    private String password;
+    private LocalDateTime lastModifiedAt;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String login, String password, LocalDateTime lastModifiedAt) {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+        this.lastModifiedAt = lastModifiedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getLastModifiedAt() {
+        return lastModifiedAt;
+    }
+
+    public void setLastModifiedAt(LocalDateTime lastModifiedAt) {
+        this.lastModifiedAt = lastModifiedAt;
+    }
+}
