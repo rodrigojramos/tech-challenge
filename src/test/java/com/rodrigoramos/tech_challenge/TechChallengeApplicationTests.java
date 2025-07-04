fa52fa6f6e7f089f71a0f4fadfa39724b2716578
Rodrigo Ramos <digojramos@gmail.com>
2025-07-04 14:12:53 -0300

    feat: initial project setup and database configuration

---
+++ src/test/java/com/rodrigoramos/tech_challenge/TechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoramos.tech_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
